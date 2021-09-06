6fbb42f6385abb968b874b37d12fc25df611dec6
carlos orellana <orellanacarlos362@gmail.com>
2021-09-06 10:06:22 -0700

    React Router Parameters

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -2,6 +2,18 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 
+function RenderCard({item}) {
+    return (
+        <Card>
+            <CardImg src={item.image} alt={item.name}/>
+            <CardBody>
+                <CardTitle>{item.name}</CardTitle>
+                <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
+
 function Home(props) {
     return (
         <div className='container'>
@@ -20,15 +32,4 @@ function Home(props) {
     )
 }
 
-function RenderCard({item}) {
-    return (
-        <Card>
-            <CardImg src={item.image} alt={item.name}/>
-            <CardBody>
-                <CardTitle>{item.name}</CardTitle>
-                <CardText>{item.description}</CardText>
-            </CardBody>
-        </Card>
-    );
-}
 export default Home;
